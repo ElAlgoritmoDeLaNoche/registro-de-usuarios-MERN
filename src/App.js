94bc47a755dc23727af05b0a5dd496030c688c3e
FranciscoN11 <fnavarrete@voxpop.com.mx>
2023-11-27 18:50:33 -0600

    Registro de usuarios MERN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Home from './components/Home'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
+
+  const user = localStorage.getItem('token')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        {
+          user && <Route path='/' exact element={<Home />} />
+        }
+        <Route path='/signup' exact element={<Signup />} />
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/' element={<Navigate replace to='/login' />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
